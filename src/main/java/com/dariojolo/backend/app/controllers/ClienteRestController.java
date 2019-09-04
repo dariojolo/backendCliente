43b4ce02f557db1e3023c689a2e59ced78f1b335
Dario Jolo <dariojolo@gmail.com>
2019-09-04 17:45:52 -0300

    Se agregaron los metodos del CRUD al controlador rest

--- src/main/java/com/dariojolo/backend/app/controllers/ClienteRestController.java
+++ src/main/java/com/dariojolo/backend/app/controllers/ClienteRestController.java
@@ -3,9 +3,16 @@ package com.dariojolo.backend.app.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dariojolo.backend.app.models.entity.Cliente;
@@ -22,5 +29,30 @@ public class ClienteRestController {
 	@GetMapping("/clientes")
 	public List<Cliente>index(){
 		return clienteService.findAll();
+	}	
+	@GetMapping("/clientes/{id}")
+	public Cliente show (@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	@PutMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente update (@RequestBody Cliente cliente, @PathVariable Long id) {
+		Cliente cl = clienteService.findById(id);
+		cl.setNombre(cliente.getNombre());
+		cl.setApellido(cliente.getApellido());
+		cl.setEmail(cliente.getEmail());
+		return clienteService.save(cl);
+	}
+	
+	@DeleteMapping("/clientes/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		clienteService.delete(id);
 	}
 }
